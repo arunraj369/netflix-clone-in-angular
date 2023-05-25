fdb5232a0dadb37d0777006826feae6c77a2e6be
Arunraj <102640467+arunraj369@users.noreply.github.com>
2023-05-25 14:35:54 +0530

    netflix clone in angular

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+})
+export class SearchComponent {
+  constructor(private service: MovieApiServiceService) {}
+
+  searchResult: any;
+  searchForm = new FormGroup({
+    movieName: new FormControl(null),
+  });
+
+  submitForm() {
+    console.log(this.searchForm.value, 'serchform value');
+    this.service.getSearchMovie(this.searchForm.value).subscribe((result) => {
+      console.log(result, 'searchmovie value');
+      this.searchResult = result.results;
+    });
+  }
+}
